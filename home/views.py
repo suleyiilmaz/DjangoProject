c51faad4a8c4c790813d10ee1753df798e14d036
suleyiilmaz <suleyilmaz10@gmail.com>
2020-05-15 02:22:38 +0300

    slider and category menu

--- home/views.py
+++ home/views.py
@@ -4,12 +4,18 @@ from django.shortcuts import render
 
 # Create your views here.
 from home.models import Setting, ContactForm, ContactFormMessage
+from product.models import Product, Category
 
 
 def index(request):
     setting= Setting.objects.get(pk=1)
-    text="Django Kurulumu: python -m pip installl -e django <br> Proje olusturma: django-admin startproject mysite <br> App Ekleme: python manage.py startapp polls "
-    context = {'setting': setting, 'page':'home'}
+    sliderdata = Product.objects.all()[:1]
+    category = Category.objects.all()
+
+    context = {'setting': setting,
+               'category': category,
+               'page':'home',
+               'sliderdata': sliderdata}
     return render(request, 'index.html', context)
 
 def aboutus(request):
